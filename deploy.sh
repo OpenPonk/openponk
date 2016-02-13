f72ceabe6a3375a490a548a3eb492ed40ddd73e1
Peter Uhnak <i.uhnak@gmail.com>
2016-02-13 17:47:30 +0100

    Playing with nicer deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -o nounset
 set -o errexit
 set -v
 
+source "smalltalkCI-master/helpers.sh"
 
 ### TEMP
 
@@ -13,23 +14,24 @@ if [[ 1 -eq 0 ]]; then
 	readonly SMALLTALK_CI_VM=smalltalkCI-master/_cache/vms/Pharo-5.0/pharo
 	readonly SMALLTALK_CI_VMS=smalltalkCI-master/_cache/vms
 	readonly TRAVIS_BUILD_NUMBER=20
+	readonly TRAVIS_BRANCH=master
 fi
 
 ###
 
 
-readonly DEPLOY_DIR="$SMALLTALK_CI_BUILD/deploy"
 readonly DEPLOY_NAME="dynacase"
+readonly DEPLOY_DIR="$SMALLTALK_CI_BUILD/deploy/$DEPLOY_NAME"
 readonly SSH_IDENTITY=.ssh/id_dynacase_travis
 readonly DEPLOY_TARGET="dynacase@ccmi.fit.cvut.cz:~/www/builds/"
 
 prepare_deploy() {
 	if [[ ! -d $DEPLOY_DIR ]]; then
-		mkdir "$DEPLOY_DIR"
+		mkdir -p "$DEPLOY_DIR"
 	fi
 	cat > "$DEPLOY_DIR/run.sh" << EOF
 #!/bin/sh
-readonly VM_UI="$(basename $(dirname "$SMALLTALK_CI_VM"))/pharo-ui"
+readonly VM_UI="./vms/Pharo-5.0/pharo-ui"
 \$VM_UI $DEPLOY_NAME.image
 EOF
 	chmod a+x $DEPLOY_DIR/run.sh
@@ -45,17 +47,26 @@ prepare_vms() {
 }
 
 deploy() {
-	cd $DEPLOY_DIR
+	prepare_deploy
+	prepare_image
+	prepare_vms
+
+	cd $DEPLOY_DIR/..
 	local build_zip="${DEPLOY_NAME}-${TRAVIS_BUILD_NUMBER}.zip"
-	zip -r "$build_zip" "run.sh" "$DEPLOY_NAME.image" "$DEPLOY_NAME.changes" "$(basename "$SMALLTALK_CI_VMS")"
+	zip -r "$build_zip" "$DEPLOY_NAME"
 	scp -rp "$build_zip" "$DEPLOY_TARGET"
 }
 
 main() {
-	prepare_deploy
-	prepare_image
-	prepare_vms
-	deploy
+	local exit_status=0
+	travis_fold start deploy "Deployment"
+	deploy || exit_status=$?
+	travis_fold end deploy
+	if [[ "$exit_status" -ne 0 ]]; then
+		print_error "Failed to deploy build."
+		exit $exit_status
+	fi
+	print_success "Build ${DEPLOY_NAME}-${TRAVIS_BUILD_NUMBER} deployed."
 }
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then

2a8bf36793f060379188aac1ab7b8856b0e99de7
Peter Uhnak <i.uhnak@gmail.com>
2016-06-06 19:51:20 +0200

    Added coverage script. Updated deploy script & travis config.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,77 +1,36 @@
 #!/bin/bash
-set -o errexit
 
-if [[ -z $TRAVIS ]]; then
-	readonly SMALLTALK_CI_HOME="smalltalkCI-master"
-	readonly SMALLTALK_CI_BUILD="$SMALLTALK_CI_HOME/_builds/$(ls smalltalkCI-master/_builds | sort -n | head -n 1)"
-	readonly SMALLTALK_CI_IMAGE="$SMALLTALK_CI_BUILD/TravisCI.image"
-	readonly SMALLTALK_CI_CHANGES="$SMALLTALK_CI_BUILD/TravisCI.changes"
-	readonly SMALLTALK_CI_VM=$SMALLTALK_CI_HOME/_cache/vms/Pharo-5.0/pharo
-	readonly SMALLTALK_CI_VMS=$SMALLTALK_CI_HOME/_cache/vms
-	readonly TRAVIS_BUILD_NUMBER=20
-	readonly TRAVIS_BRANCH=master
-	readonly TRAVIS_BUILD_DIR=.
-	readonly TRAVIS=true
-fi
+# exit on first encountered error
+set -o errexit
 
 source "$SMALLTALK_CI_HOME/helpers.sh"
 
-readonly DEPLOY_NAME="dynacase"
-readonly DEPLOY_DIR="$SMALLTALK_CI_BUILD/deploy/$DEPLOY_NAME"
-readonly SSH_IDENTITY=.ssh/id_dynacase_travis
-readonly DEPLOY_MACHINE="dynacase@ccmi.fit.cvut.cz"
-readonly DEPLOY_TARGET_DIR="~/www/builds/all-in-one"
-readonly DEPLOY_TARGET="$DEPLOY_MACHINE:$DEPLOY_TARGET_DIR"
+# wherever you'll be ssh-ing into user@machine
+readonly TARGET_MACHINE="dynacase@ccmi.fit.cvut.cz"
+# target dir on the target machine
+readonly TARGET_DIR="~/www/builds/all-in-one"
 
-prepare_deploy() {
-	if [[ ! -d $DEPLOY_DIR ]]; then
-		mkdir -p "$DEPLOY_DIR"
-	fi
-	cp $TRAVIS_BUILD_DIR/scripts/README.txt $DEPLOY_DIR
-}
+# customize the name of the Pharo image you will be deploying
+readonly PROJECT_NAME="dynacase"
+# customize the name of the build folder
+readonly ARTIFACT_DIR="${PROJECT_NAME}-image-${TRAVIS_BUILD_NUMBER}"
 
-copy_image() {
-	cp "$SMALLTALK_CI_IMAGE" "$DEPLOY_DIR/$DEPLOY_NAME.image"
-	cp "$SMALLTALK_CI_CHANGES" "$DEPLOY_DIR/$DEPLOY_NAME.changes"
-}
+# Rename the image, zip it, and send it to a server
+deploy-simple() {
+	mkdir "$ARTIFACT_DIR"
+	cp "$SMALLTALK_CI_IMAGE" "${ARTIFACT_DIR}/${PROJECT_NAME}.image"
+	cp "$SMALLTALK_CI_CHANGES" "${ARTIFACT_DIR}/${PROJECT_NAME}.changes"
 
-run_in_image() {
-	local cmd=$1
-	print_info "$cmd"
-	timer_start
-	$SMALLTALK_CI_VM --nodisplay $DEPLOY_DIR/$DEPLOY_NAME.image eval --save "$cmd"
-	timer_finish
-}
-
-postprocess_image() {
-	# this is mainly to drive the overall size down
-	run_in_image 'Smalltalk cleanUp: true'
-	#run_in_image 'PharoChangesCondenser condense'
-	rm -f $DEPLOY_DIR/$DEPLOY_NAME.changes.bak
-}
-
-deploy() {
-	prepare_deploy
-	copy_image
-	postprocess_image
-
-	cd $DEPLOY_DIR/..
-	local build_zip="${DEPLOY_NAME}-image-${TRAVIS_BUILD_NUMBER}.zip"
-	zip -qr "$build_zip" "$DEPLOY_NAME"
-	scp -rp "$build_zip" "$DEPLOY_TARGET"
-	ssh "$DEPLOY_MACHINE" "~/bundler/post-deploy.sh ${TRAVIS_BUILD_NUMBER}"
+	local build_zip="${ARTIFACT_DIR}.zip"
+	zip -qr "$build_zip" "$ARTIFACT_DIR"
+	scp -rp "$build_zip" "$TARGET_MACHINE:$TARGET_DIR"
+	# I have a server-side post-processing script that bundles VMs into the build
+	ssh "$TARGET_MACHINE" "~/bundler/post-process.sh ${TRAVIS_BUILD_NUMBER}"
 }
 
 main() {
-	local exit_status=0
-	travis_fold start deploy "Deployment"
-	deploy || exit_status=$?
-	travis_fold end deploy
-	if [[ "$exit_status" -ne 0 ]]; then
-		print_error "Failed to deploy build."
-		exit $exit_status
-	fi
-	print_success "Build ${DEPLOY_NAME}-${TRAVIS_BUILD_NUMBER} deployed."
+	deploy-simple
+	echo "Build ${ARTIFACT_DIR} deployed."
 }
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
